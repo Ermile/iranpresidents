a6bb385a4a71126aa5131ebb6bc556c5cc3765a5
Reza <rm.biqarar@gmail.com>
2018-02-13 11:10:03 +0330

    fix translation

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -1,6 +1,203 @@
 <?php
-private function transtext()
+class twigTransTerms
 {
+ private function transtext()
+ {
 
+	//-------------------a/project/iranpresidents/includes/languages/trans_static.html
+	echo T_("Jibres");                                                                // Line 7 Seperate
+	echo T_(" ");                                                                     // Line 11 Seperate
+	echo T_("money");                                                                 // Line 13 Seperate
+	echo T_("toman");                                                                 // Line 14 Seperate
+
+	//a/project/iranpresidents/public_html/static/siftal/fonts/siftal/icons-reference.html
+
+	//--------------------------------------------------------------------display.html
+	echo T_("Election list");                                                         // Line 5
+	echo T_("Title");                                                                 // Line 66
+	echo T_("Status");                                                                // Line 9
+	echo T_("Eligible");                                                              // Line 149
+	echo T_("Voted");                                                                 // Line 78
+	echo T_("Invalid");                                                               // Line 82
+	echo T_("Cash");                                                                  // Line 13
+	echo T_("Branchs");                                                               // Line 90
+	echo T_("First vote male count");                                                 // Line 15
+	echo T_("First vote female count");                                               // Line 16
+	echo T_("Signuped");                                                              // Line 102
+	echo T_("Verified");                                                              // Line 153
+	echo T_("Candida count");                                                         // Line 19
+	echo T_("Start time");                                                            // Line 114
+	echo T_("End_time");                                                              // Line 21
+	echo T_("Election date");                                                         // Line 122
+	echo T_("Jalali year");                                                           // Line 126
+	echo T_("Year");                                                                  // Line 41
+	echo T_("En url");                                                                // Line 134
+	echo T_("Fa url");                                                                // Line 138
+	echo T_("Cat");                                                                   // Line 142
+	echo T_("Win");                                                                   // Line 145
+	echo T_("Link");                                                                  // Line 50
+	echo T_("Edit election data");                                                    // Line 59
+	echo T_("Add new election");                                                      // Line 61
+	echo T_("En Title");                                                              // Line 70
+	echo T_("Cashs");                                                                 // Line 86
+	echo T_("First vote male");                                                       // Line 94
+	echo T_("First vote female");                                                     // Line 98
+	echo T_("Candida");                                                               // Line 8
+	echo T_("End time");                                                              // Line 118
+	echo T_("Please select one item");                                                // Line 44
+	echo T_("Desc");                                                                  // Line 20
+	echo T_("status");                                                                // Line 11
+	echo T_("awaiting");                                                              // Line 160
+	echo T_("running");                                                               // Line 161
+	echo T_("done");                                                                  // Line 162
+	echo T_("Save");                                                                  // Line 91
+	echo T_("Update");                                                                // Line 88
+
+	//---------------------a/project/iranpresidents/content/admin/candida/display.html
+	echo T_("Candida List");                                                          // Line 5
+	echo T_("election");                                                              // Line 42
+	echo T_("Name");                                                                  // Line 65
+	echo T_("En Name");                                                               // Line 69
+	echo T_("Family");                                                                // Line 75
+	echo T_("En Family");                                                             // Line 79
+	echo T_("Father");                                                                // Line 84
+	echo T_("En Father");                                                             // Line 88
+	echo T_("Fame");                                                                  // Line 92
+	echo T_("En Fame");                                                               // Line 96
+	echo T_("Nationalcode");                                                          // Line 17
+	echo T_("Brith date");                                                            // Line 18
+	echo T_("Election code");                                                         // Line 108
+	echo T_("File_url");                                                              // Line 21
+	echo T_("File_url_2");                                                            // Line 22
+	echo T_("Win url");                                                               // Line 23
+	echo T_("Edit candida data");                                                     // Line 49
+	echo T_("Add new candida");                                                       // Line 51
+	echo T_("election_id");                                                           // Line 55
+	echo T_("name");                                                                  // Line 15
+	echo T_("en_name");                                                               // Line 70
+	echo T_("family");                                                                // Line 16
+	echo T_("fame");                                                                  // Line 91
+	echo T_("birthdate");                                                             // Line 19
+	echo T_("Birth date");                                                            // Line 100
+	echo T_("nationalcode");                                                          // Line 18
+	echo T_("National code");                                                         // Line 104
+	echo T_("electioncode");                                                          // Line 20
+	echo T_("file_url");                                                              // Line 111
+	echo T_("file_url_2");                                                            // Line 115
+	echo T_("win_url");                                                               // Line 119
+	echo T_("active");                                                                // Line 125
+	echo T_("cancel");                                                                // Line 126
+	echo T_("desc");                                                                  // Line 82
+
+	//-----------------------------a/project/iranpresidents/content/admin/display.html
+	echo T_("Admin Election");                                                        // Line 5
+	echo T_("Election");                                                              // Line 7
+
+	//---------------------------------a/project/iranpresidents/content/home/home.html
+	echo T_("Presidents of Islamic Republic of Iran");                                // Line 12
+	echo T_("All Elections");                                                         // Line 61
+	echo T_("All Candidates");                                                        // Line 62
+	echo T_("Turnout");                                                               // Line 42
+	echo T_("President elected");                                                     // Line 105
+	echo T_("Percentage");                                                            // Line 44
+	echo T_("Popular vote");                                                          // Line 107
+	echo T_("Percent of total eligible");                                             // Line 119
+	echo T_("Unknown");                                                               // Line 449
+	echo T_("Votes");                                                                 // Line 350
+
+	//------------------------------a/project/iranpresidents/content/home/candida.html
+	echo T_("All Candidates of Presidential Elections of Islamic Republic of Iran");  // Line 12
+	echo T_("Candidate");                                                             // Line 43
+
+	//------------------------------a/project/iranpresidents/content/home/comment.html
+	echo T_("Your Name");                                                             // Line 514
+	echo T_("Your name");                                                             // Line 515
+	echo T_("Your mobile number");                                                    // Line 519
+	echo T_("Your comment");                                                          // Line 522
+	echo T_("Your Comment");                                                          // Line 523
+	echo T_("Send");                                                                  // Line 527
+
+	//------------------------------a/project/iranpresidents/content/home/display.html
+	echo T_("Results of");                                                            // Line 32
+	echo T_("President-elect of");                                                    // Line 92
+	echo T_("Live");                                                                  // Line 209
+	echo T_("Last Update");                                                           // Line 212
+	echo T_("Comparison result of");                                                  // Line 366
+	echo T_("Candidates Votes on Provinces");                                         // Line 383
+	echo T_("Province");                                                              // Line 386
+	echo T_("Candidates Votes on Cities");                                            // Line 407
+	echo T_("City");                                                                  // Line 410
+
+	//-------------------a/project/iranpresidents/content/data/result/city_result.html
+	echo T_("Add city result of election");                                           // Line 3
+
+	//-----------------------a/project/iranpresidents/content/data/result/display.html
+	echo T_("id");                                                                    // Line 8
+	echo T_("total");                                                                 // Line 9
+	echo T_("title");                                                                 // Line 10
+	echo T_("father");                                                                // Line 17
+	echo T_("date");                                                                  // Line 52
+	echo T_("level");                                                                 // Line 56
+	echo T_("number");                                                                // Line 60
+	echo T_("cash");                                                                  // Line 64
+	echo T_("voted");                                                                 // Line 68
+	echo T_("invalid");                                                               // Line 72
+
+	//------------------------a/project/iranpresidents/content/data/report/result.html
+	echo T_("Report list");                                                           // Line 5
+	echo T_("Date");                                                                  // Line 47
+
+	//--------------------------a/project/iranpresidents/content/data/report/vote.html
+	echo T_("Edit");                                                                  // Line 37
+
+	//-----------------------a/project/iranpresidents/content/data/report/display.html
+	echo T_("Edit report resutl");                                                    // Line 35
+	echo T_("Add new report");                                                        // Line 37
+	echo T_("enable");                                                                // Line 77
+	echo T_("disable");                                                               // Line 78
+
+	//------------------------------a/project/iranpresidents/content/data/display.html
+	echo T_("Election List");                                                         // Line 7
+	echo T_("add result");                                                            // Line 12
+	echo T_("add city result");                                                       // Line 13
+	echo T_("En");                                                                    // Line 17
+	echo T_("Fa");                                                                    // Line 17
+	echo T_("Add report");                                                            // Line 20
+	echo T_("Add place");                                                             // Line 21
+	echo T_("Running Election");                                                      // Line 34
+
+	//---------------------------a/project/iranpresidents/content/main/layout.bak.html
+	echo T_("Sarshomar");                                                             // Line 25
+	echo T_("Admin");                                                                 // Line 28
+	echo T_("My Profile");                                                            // Line 73
+	echo T_("Change Password");                                                       // Line 74
+	echo T_("Logout");                                                                // Line 75
+	echo T_("Total Polls");                                                           // Line 89
+	echo T_("Spam Polls");                                                            // Line 99
+	echo T_("Awaiting Polls");                                                        // Line 109
+	echo T_("Draft Polls");                                                           // Line 119
+	echo T_("Polls");                                                                 // Line 126
+	echo T_("Registration");                                                          // Line 134
+	echo T_("Successful");                                                            // Line 143
+	echo T_("Guests");                                                                // Line 163
+	echo T_("Telegram");                                                              // Line 173
+	echo T_("API");                                                                   // Line 183
+	echo T_("Total");                                                                 // Line 278
+	echo T_("Unique");                                                                // Line 288
+	echo T_("Today");                                                                 // Line 297
+	echo T_("Last Week");                                                             // Line 229
+	echo T_("Visitors");                                                              // Line 241
+	echo T_("Answers");                                                               // Line 251
+	echo T_("Transactions");                                                          // Line 257
+	echo T_("Visits");                                                                // Line 266
+	echo T_("Latest Polls");                                                          // Line 307
+	echo T_("Trending Polls");                                                        // Line 325
+
+	//-------------------------------a/project/iranpresidents/content/main/layout.html
+	echo T_("Admin Dashboard");                                                       // Line 6
+	echo T_("Add Data");                                                              // Line 11
+	echo T_("View Election");                                                         // Line 13
+
+ }
 }
 ?>
