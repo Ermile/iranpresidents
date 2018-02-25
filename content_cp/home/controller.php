f4c3992457d43c2e9916d729481784e17d1c8370
Reza Mohiti <rm.biqarar@gmail.com>
2018-02-25 16:43:55 +0330

    add cp dashboard

---
+++ content_cp/home/controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace content_cp\home;
+
+
+class controller extends \addons\content_cp\home\controller
+{
+
+}
+?>
---
+++ content_cp/home/view.php
@@ -0,0 +1,9 @@
+<?php
+namespace content_cp\home;
+
+
+class view extends \addons\content_cp\home\view
+{
+
+}
+?>
