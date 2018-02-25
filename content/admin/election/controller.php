5ecb5043dcd8519be203ca6c33e0cf191ec04d89
Reza Mohiti <rm.biqarar@gmail.com>
2018-02-25 11:57:25 +0330

    fix edit election

--- content/admin/election/controller.php
+++ content/admin/election/controller.php
@@ -12,11 +12,11 @@ class controller extends \content\main\controller
 
 		\lib\permission::access('election:admin:admin', 'block');
 
-		$this->get("list", "list")->ALL();
 		$this->get("election", "election")->ALL("/admin\/election\/edit=(\d+)/");
+		$this->get("list", "list")->ALL();
 
-		$this->post('election')->ALL();
 		$this->post("edit")->ALL("/admin\/election\/edit=(\d+)/");
+		$this->post('election')->ALL();
 	}
 }
 ?>
