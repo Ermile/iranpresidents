4839c754de37eb96fe11a9d356d854bbcd4895bb
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-02-26 17:01:47 +0330

    fix site title and desc and fix trans

--- includes/mvc/view.php
+++ includes/mvc/view.php
@@ -7,9 +7,9 @@ class view extends \lib\view
 	{
 		// define default value for global
 
-		$this->data->site['title']           = T_("Iran presidents");
-		$this->data->site['desc']            = T_("Iran presidents");
-		$this->data->site['slogan']          = T_("Iran presidents");
+		$this->data->site['title']           = T_("Iran Presidents");
+		$this->data->site['desc']            = T_("Live and complete result of iran elections after revolution until now.");
+		$this->data->site['slogan']          = T_("Know more about iranian presidents");
 
 		$this->data->page['desc']            = $this->data->site['desc']. ' | '. $this->data->site['slogan'];
 
