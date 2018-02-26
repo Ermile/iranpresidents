9d013ad1b341508d24fd31c5952b4dec3cee04e7
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-02-26 16:22:06 +0330

    fix candidates page

--- content/candida/view.php
+++ content/candida/view.php
@@ -6,8 +6,8 @@ class view extends \mvc\view
 {
 	public function config()
 	{
-		$this->data->page['title'] = T_('Presidents of Islamic Republic of Iran');
-		$this->data->page['desc']  = T_('Live and complete result of iran elections after revolution until now.');
+		$this->data->page['title'] = T_('All Candidates of Presidential Elections of Islamic Republic of Iran');
+		$this->data->page['desc']  = T_('Review all candidates details');
 		$this->data->result        = \lib\candidas::get_list_all('president');
 	}
 }
