529d1028441497fa387c410e08dac12ffdf58d77
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-06 04:00:43 +0430

    fix main mvc

---
+++ content/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content;
+
+class view
+{
+	public static function config()
+	{
+		\lib\data::site_title(T_("Iran Presidents"));
+		\lib\data::site_desc(T_("Live and complete result of iran elections after revolution until now."));
+		\lib\data::site_slogan(T_("Know more about iranian presidents"));
+	}
+}
+?>
